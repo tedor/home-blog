41e6cde964b9501864925f17d496ffea1fd0e770
tedor <vetalyi@gmail.com>
2015-01-12 01:43:01 +0200

    one feeds for all blog

--- content/feeds.py
+++ content/feeds.py
@@ -1,24 +1,38 @@
 from django.contrib.syndication.views import Feed
+from blog.models import Post
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
-from content.views import get_all_data
 from content.utils import get_site_name
 
 class LatestEntriesFeed(Feed):
-    title = "%s - %s" % (get_site_name(), unicode(_("all feeds")))
-    description = _("Latest news from all pages")
-    
+    title = "%s - %s" % (get_site_name(), unicode(_("blog")))
+    description = _("Latest posts from blog page")
+
     def link(self):
-        return reverse('home_feeds')
-        
+        return reverse('blog_feeds')
+
     def items(self):
-        return get_all_data()
-    
+        return Post.objects.filter(status=Post.STATUS_PUBLIC).order_by('-created_at')[:10]
+
     def item_title(self, item):
-        return item['title']
+        return item.title
 
     def item_description(self, item):
-        return item['text']
-    
+        return item.get_text_cut
+
     def item_link(self, item):
-        return item['url']
+        return item.get_absolute_url()
+
+    def item_pubdate(self, item):
+	    return item.updated_at
+
+class LatestTagEntriesFeed(LatestEntriesFeed):
+    tag = ''
+
+    def get_object(self, request, tag):
+        self.tag = tag
+        self.description = _("Latest posts from blog page by tag") + ': ' + tag;
+        self.title = "%s - %s" % (get_site_name(), unicode(_("blog"))) + ': ' + tag;
+
+    def items(self):
+        return Post.objects.filter(status=Post.STATUS_PUBLIC, tag__contains=self.tag).order_by('-created_at')[:10]
